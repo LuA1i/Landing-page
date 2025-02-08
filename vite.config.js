818e4e704e50d9dc9fdfbe50509efcee941dd85f
Luqman Ali <luqman.ali270304@outlook.com>
2025-02-08 08:39:55 +0000

    fixed typo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/landing-page/',
+  base: '/Landing-page/',
   plugins: [react(), tailwindcss()],
 })
